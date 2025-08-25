868dde1f583a3f1588863c0154b7106ef9fcb583
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2025-08-25 10:42:01 +0200

    började

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Meny:");
+        System.out.println("Addition:");
+        System.out.println("Subtraktion:");
+        System.out.println("Multiplikation:");
+        System.out.println("division:");
+
+
+    double num1;
+    double num2;
+    char tecken;          
+    double result;
+
+        scanner.close();
+    }
+}
